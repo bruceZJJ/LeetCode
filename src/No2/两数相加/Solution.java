69226786fcbc771fd431fb370ae95aa62d6c18d6
bruceZJJ <zjj510855282@qq.com>
2018-12-08 20:13:40 +0800

    2018/12/8 测试提交4

--- src/No2/两数相加/Solution.java
+++ src/No2/两数相加/Solution.java
@@ -27,6 +27,6 @@ package No2.两数相加;
  */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return new ListNode(3);
+        return new ListNode(4);
     }
 }
