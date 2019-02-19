37a789c5c29152dbb30e5e95696c77f3e3f4753a
bruceZJJ <zjj510855282@qq.com>
2019-02-19 21:17:52 +0800

    2019/02/19 No2两数相加

--- src/No2/两数相加/Solution.java
+++ src/No2/两数相加/Solution.java
@@ -27,6 +27,27 @@ package No2.两数相加;
  */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return new ListNode(5);
+        ListNode l3 = new ListNode(0);
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        ListNode p3 = l3;
+        int sum = 0;
+        while (p1 != null || p2 != null) {
+            if (p1 != null) {
+                sum += p1.val;
+                p1 = p1.next;
+            }
+            if (p2 != null) {
+                sum += p2.val;
+                p2 = p2.next;
+            }
+            p3.next = new ListNode(sum % 10);
+            sum /= 10;
+            p3 = p3.next;
+        }
+        if (sum == 1) {
+            p3.next = new ListNode(1);
+        }
+        return l3.next;
     }
 }
